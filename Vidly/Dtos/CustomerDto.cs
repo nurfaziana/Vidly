d0789820753f68ce72fe9dbe948b55f4d31bf9e7
nurfaziana <nurfazianas@gmail.com>
2018-04-10 11:32:52 +0800

    Commit css

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly2.Models;
+
+namespace Vidly2.Dtos
+{
+    public class CustomerDto
+    {
+        public int ID { get; set; }
+        [Required]   // 1. add data annotation
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+       
+        public byte MembershipTypeId { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
